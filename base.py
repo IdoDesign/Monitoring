d21f9ebcbca83e1568051b580379ef27d7898de7
IdoDesign <idodesignil@gmail.com>
2021-12-24 15:14:31 +0200

    using .env file for db

--- base.py
+++ base.py
@@ -1,14 +1,16 @@
-from configparser import ConfigParser
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-config = ConfigParser()
-config.read('config.ini')
-db_config = config['POSTGRES']
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+POSTGRES_HOST = os.getenv('SQL_HOST')
+POSTGRES_DB = os.getenv('POSTGRES_DB')
+SQL_PORT = os.getenv('SQL_PORT')
 
 engine = create_engine(
-    'postgresql+psycopg2://{}:{}@{}:5432/{}'.format(db_config['POSTGRES_USER'], db_config['POSTGRES_PASSWORD'], db_config['POSTGRES_HOST'], db_config['POSTGRES_DB']))
+    'postgresql+psycopg2://{}:{}@{}:{}/{}'.format(POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, SQL_PORT, POSTGRES_DB))
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
